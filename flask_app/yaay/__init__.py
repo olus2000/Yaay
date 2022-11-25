d5a37af214e275368a6c8d8cee9e0522b27947e2
olus2000 <alsabak@gmail.com>
2022-11-25 20:27:50 +0100

    Flask app skeleton

---
+++ flask_app/yaay/__init__.py
@@ -0,0 +1,45 @@
+import os
+import os.path
+import re
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# from yaay import names, of, blueprints
+# from yaay.model import model, stuff
+# from yaay.cli import cli, stuff
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+
+    # ensure the instance folder exists
+    if not os.path.exists(app.instance_path):
+        os.mkdir(app.instance_path)
+
+    # load config and overwrite if testing
+    app.config.from_pyfile('config.py', silent=True)
+    if test_config is not None:
+        app.config.config_mapping(test_config)
+
+    # database connection TODO
+
+    # simple page
+    @app.route('/hello')
+    def hello():
+        return 'Henlo ,warld!'
+
+    # pages
+    # app.register_blueprint(some.bp)
+
+    # model
+    # ap.register_blueprint(some.bp)
+
+    # commandline interface
+    # app.cli.add_command(command)
+
+    return app
+
+
+app = create_app()
