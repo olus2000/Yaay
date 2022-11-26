614e67f185c567e172f47c590fde0ab72c2b8a1d
Tomasz Świderski <01167294@pw.edu.pl>
2022-11-26 10:02:26 +0100

    database even more populated

--- flask_app/yaay/cli.py
+++ flask_app/yaay/cli.py
@@ -20,16 +20,7 @@ def init_db(reset):
     db.create_all()
 
     #initializing database
-    db.session.add(Event(info="Hackathon Goldman Sachs", stage_amount=5))
-    db.session.add(Event(info="PW Work Trades", stage_amount=3))
-    db.session.add(Event(info="Studen Debil Day", stage_amount=2))
-    db.session.add(Task(filename="bit_sum.TXT", answer="2137", title="Bit Sum"))
-    db.session.add(Task(filename="fib.TXT", answer="alot", title="Fibonacci"))
-    db.session.add(Task(filename="median.TXT", answer="mediana", title="Mediana"))
-    db.session.add(EventTask(event_id=3, task_id="bit_sum.TXT"))
-    db.session.add(EventTask(event_id=3, task_id="fib.TXT"))
-    db.session.add(EventTask(event_id=3, task_id="median.TXT"))
-    db.session.commit()
+    initialize_database()
 
     print('Database initialised.')
 
@@ -91,9 +82,9 @@ def add_task_to_event(filename, event_id):
 
 
 @click.command('show')
-@click.option('--task', default=False)
-@click.option('--event', default=False)
-@click.option('--event-task', default=False)
+@click.option('--task', default=False, is_flag=True)
+@click.option('--event', default=False, is_flag=True)
+@click.option('--event-task', default=False, is_flag=True)
 @with_appcontext
 def show(task, event, event_task):
     if task:
@@ -102,3 +93,28 @@ def show(task, event, event_task):
         print(Event.query.all())
     if event_task:
         print(EventTask.query.all())
+
+def initialize_database():
+    db.session.add(Event(info="Hackathon Goldman Sachs", stage_amount=3))
+    db.session.add(Event(info="PW Job Fairs", stage_amount=3))
+    db.session.add(Event(info="Women in Tech", stage_amount=3))
+
+    db.session.add(Task(filename="bit_sum.TXT", answer="2137", title="Bit Sum"))
+    db.session.add(Task(filename="fib.TXT", answer="68245678", title="Fibonacci"))
+    db.session.add(Task(filename="median.TXT", answer="median", title="Median"))
+    db.session.add(Task(filename="employees.TXT", answer="43900", title="Employees"))
+    db.session.add(Task(filename="foundation.TXT", answer="1869", title="Foundation"))
+    db.session.add(Task(filename="sector.TXT", answer="banking", title="Sector"))
+    db.session.add(Task(filename="sum.TXT", answer="56", title="Sum"))
+    db.session.add(Task(filename="headquarter.TXT", answer="New York", title="Headquarter"))
+
+    db.session.add(EventTask(event_id=2, task_id="bit_sum.TXT"))
+    db.session.add(EventTask(event_id=2, task_id="fib.TXT"))
+    db.session.add(EventTask(event_id=2, task_id="median.TXT"))
+    db.session.add(EventTask(event_id=2, task_id="sum.TXT"))
+    db.session.add(EventTask(event_id=1, task_id="employees.TXT"))
+    db.session.add(EventTask(event_id=1, task_id="sector.TXT"))
+    db.session.add(EventTask(event_id=1, task_id="foundation.TXT"))
+    db.session.add(EventTask(event_id=1, task_id="headquarter.TXT"))
+
+    db.session.commit()
