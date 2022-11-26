dd0ed06ce4ff75e42900cd8a889643b3139b0aca
Łukasz Główka <glowka36875@gmail.com>
2022-11-26 06:33:38 +0100

    added argparse argument

--- brochure/brochure.py
+++ brochure/brochure.py
@@ -25,17 +25,18 @@ def create_qr(text, qr_settings=QR):
     return img
 
 
-def create_brochure(qr_img):
+def create_brochure(qr_img, event):
     with Image.open(IMAGE_PATH) as img:
         img_w, img_h = img.size
         qr_w, qr_h = qr_img.size
         img.paste(qr_img, (img_w - qr_w - 150, img_h - qr_h - 100))
-        img.save(IMAGE_FOLDER_PATH + 'brochure.png')
+        img.save(IMAGE_FOLDER_PATH + f'brochure_{event}.png')
         return img
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--text', type=str)
+    parser.add_argument('-e', '--event', type=str)
     args = parser.parse_args()
-    create_brochure(create_qr(args.text))
+    create_brochure(create_qr(args.text), args.event)
